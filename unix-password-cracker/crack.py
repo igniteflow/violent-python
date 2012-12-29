9026dc4e28a7d641da670081885b53e650e3c575
tysoep <tysoep@redmist-2.(none)>
2012-12-29 23:25:26 +0000

    Added threaded and pool options

--- unix-password-cracker/crack.py
+++ unix-password-cracker/crack.py
@@ -1,5 +1,9 @@
+import argparse
 import crypt
 import threading
+
+from multiprocessing import Pool
+
 
 """
 UNIX password cracker
@@ -12,27 +16,62 @@ dictionary.txt (one word per line example file from http://www.math.sjsu.edu/~fo
 
 
 def test_pass(user, crypt_pass, dict_words):
-	salt = crypt_pass[0:2]
-	for word in dict_words:
-		crypt_word = crypt.crypt(word, salt)
-		if crypt_word.strip() == crypt_pass.strip():
-			print 'Password for %s is: %s \n' % (user, word)
-			return
-	print 'Password for %s not found \n' % user 
+    salt = crypt_pass[0:2]
+    for word in dict_words:
+        crypt_word = crypt.crypt(word, salt)
+        if crypt_word.strip() == crypt_pass.strip():
+            return 'Password for %s is: %s' % (user, word)
+            return
+    return 'Password for %s not found' % user 
 
-def main():
-	dictionary = 'dictionary.txt'
-	with open(dictionary, 'r') as f:
-		dict_words = [line.strip('\n').strip() for line in f.readlines()]
-	
-	passwords = 'passwords.txt'
-	with open(passwords, 'r') as f:
-		for line in f.readlines():
-			if ":" in line:
-				user = line.split(':')[0]
-				crypt_pass = line.split(':')[1].strip(' ')
-				t = threading.Thread(target=test_pass, args=[user, crypt_pass, dict_words])
-				t.start()
-			
+def print_pass(x):
+    print x
+
+def use_threading(func, args):
+    t = threading.Thread(target=test_pass, args=args)
+    t.start()
+    return t
+    
+def use_multithreaded_pools(func, args):
+    pool = Pool(processes=10)
+    return pool.apply_async(func, args)
+    
+def main(mode=None):
+    dictionary = 'dictionary.txt'
+    with open(dictionary, 'r') as f:
+        dict_words = [line.strip('\n').strip() for line in f.readlines()]
+    
+    results = [] # used in 'pool' mode only
+
+    passwords = 'passwords.txt'
+    with open(passwords, 'r') as f:
+        for line in f.readlines():
+            if ":" in line:
+                user = line.split(':')[0]
+                crypt_pass = line.split(':')[1].strip(' ')
+                
+                if mode == 'threading':
+                    results.append(use_threading(test_pass, [user, crypt_pass, dict_words]))
+                elif mode == 'pool':
+                    results.append(use_multithreaded_pools(test_pass, [user, crypt_pass, dict_words]))
+                else:
+                    # default - execute sequentially 
+                    results.append(test_pass(user, crypt_pass, dict_words))
+        
+    if results:     
+        for result in results:
+            if mode == 'threading':
+                pass
+            elif mode == 'pool':
+                print result.get()
+            else:
+                print result
+
 if __name__ == "__main__":
-	main()
+    mode = None
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", help="increase output verbosity")
+    args = parser.parse_args()
+    if args.mode:
+        mode = args.mode
+    main(mode=mode)
