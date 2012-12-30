97d162fc5f77df70e71b3335ed6e4433ff980c8c
tysoep <tysoep@redmist-2.(none)>
2012-12-30 00:23:44 +0000

    Fixed up a few bugs

--- unix-password-cracker/crack.py
+++ unix-password-cracker/crack.py
@@ -2,76 +2,81 @@ import argparse
 import crypt
 import threading
 
-from multiprocessing import Pool
+from multiprocessing import Pool, Queue
 
-
-"""
-UNIX password cracker
-Uses a simple dictionary based brute-force attack to guess a user's
-password by hashing the dictionary word and then checking for equality
-against the existing hash.  
-passwords.txt (as per /etc/passwd)
-dictionary.txt (one word per line example file from http://www.math.sjsu.edu/~foster/dictionary.txt)
-"""
-
+queue = Queue()
 
 def test_pass(user, crypt_pass, dict_words):
+    """Standalone function as multithreading doesn't appear to like instance methods"""
     salt = crypt_pass[0:2]
     for word in dict_words:
         crypt_word = crypt.crypt(word, salt)
         if crypt_word.strip() == crypt_pass.strip():
-            return 'Password for %s is: %s' % (user, word)
+            queue.put('Password for %s is: %s' % (user, word))
             return
-    return 'Password for %s not found' % user 
+    queue.put('Password for %s not found' % user)
 
-def print_pass(x):
-    print x
 
-def use_threading(func, args):
-    t = threading.Thread(target=test_pass, args=args)
-    t.start()
-    return t
-    
-def use_multithreaded_pools(func, args):
-    pool = Pool(processes=10)
-    return pool.apply_async(func, args)
-    
-def main(mode=None):
-    dictionary = 'dictionary.txt'
-    with open(dictionary, 'r') as f:
-        dict_words = [line.strip('\n').strip() for line in f.readlines()]
-    
-    results = [] # used in 'pool' mode only
+class UnixPasswordCracker(object):
+    """
+    Uses a simple dictionary based brute-force attack to guess a user's
+    password by hashing the dictionary word and then checking for equality
+    against the existing hash.  
+    passwords.txt (as per /etc/passwd)
+    dictionary.txt (one word per line example file from http://www.math.sjsu.edu/~foster/dictionary.txt)
+    """
+    pool = Pool(processes=5)
 
-    passwords = 'passwords.txt'
-    with open(passwords, 'r') as f:
-        for line in f.readlines():
-            if ":" in line:
-                user = line.split(':')[0]
-                crypt_pass = line.split(':')[1].strip(' ')
-                
-                if mode == 'threading':
-                    results.append(use_threading(test_pass, [user, crypt_pass, dict_words]))
-                elif mode == 'pool':
-                    results.append(use_multithreaded_pools(test_pass, [user, crypt_pass, dict_words]))
-                else:
-                    # default - execute sequentially 
-                    results.append(test_pass(user, crypt_pass, dict_words))
+    def use_threading(self, func, args):
+        t = threading.Thread(target=func, args=args)
+        t.start()
+        t.join()
         
-    if results:     
-        for result in results:
-            if mode == 'threading':
-                pass
-            elif mode == 'pool':
-                print result.get()
-            else:
-                print result
+    def use_multithreaded_pools(self, func, args):
+        return self.pool.apply_async(func, args)
+        
+    def main(self, mode=None):
+        dictionary = 'dictionary.txt'
+        with open(dictionary, 'r') as f:
+            dict_words = [line.strip('\n').strip() for line in f.readlines()]
+        
+        passwords = 'passwords.txt'
+        with open(passwords, 'r') as f:
+            for line in f.readlines():
+                if ":" in line:
+                    user = line.split(':')[0]
+                    crypt_pass = line.split(':')[1].strip(' ')
+                    args = [user, crypt_pass, dict_words]
+                    
+                    if mode == 'threading':
+                        self.use_threading(test_pass, args)
+                    elif mode == 'pool':
+                        self.use_multithreaded_pools(test_pass, args)
+                    else:
+                        test_pass(*args)
+
+        # close the pool and wait for the workers to finish
+        self.pool.close()
+        self.pool.join()        
+
+        # print the queue items
+        while not queue.empty():
+            print queue.get()
+        
 
 if __name__ == "__main__":
+    """
+    Performance (from `time` real):
+        default (sequential) = 0m8.315s
+        threading = 0m8.228s
+        pool = 0m6.950s
+    @todo These times really should be averages from multiple runs, however they are not, 
+    because I am lazy
+    """
     mode = None
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", help="increase output verbosity")
+    parser.add_argument("--mode", help="Valid choices: 'pool' and 'threading'")
     args = parser.parse_args()
     if args.mode:
         mode = args.mode
-    main(mode=mode)
+    UnixPasswordCracker().main(mode=mode)
